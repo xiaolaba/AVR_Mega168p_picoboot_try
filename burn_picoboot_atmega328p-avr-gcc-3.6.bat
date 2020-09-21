96f7f3efd8e69196f058d8042eb1811619c7cdcf
xiaolaba <xiao_laba_cn@yahoo.com>
2020-09-21 08:34:33 +0800

    Add files via upload

--- burn_picoboot_atmega328p-avr-gcc-3.6.bat
+++ burn_picoboot_atmega328p-avr-gcc-3.6.bat
@@ -7,16 +7,24 @@
 set mcu=atmega328p
 set hex=picobootArduino328v3b2.hex
 
-:: Atmega168P, BOD 1.8V, Arduino bootloader
+:: Atmega168P, BOD 1.8V, Arduino bootloader, picoboot 250000 baud rate, tested ok
 ::set lfuse=0xF7
 ::set hfuse=0xd6
 ::set efuse=0xfc
 
 
-:: Atmega328P, optiboot bootloader
+:: Atmega328P, optiboot bootloader, Nano, baud rate 250000, tested ok
 ::set lfuse=0xFF
 ::set hfuse=0xda
 ::set efuse=0xfd
+
+
+
+:: Atmega328P, picoboot bootloader, UNO, baud rate 250000 baud, tested ok
+set lfuse=0xF7
+set hfuse=0xde
+set efuse=0xff
+
 
 
 ::set ac=C:\WinAVR-20100110
@@ -28,9 +36,9 @@ path %ac%\bin;%ac%\utils\bin;%path%;
 avr-gcc.exe -dumpversion
 
 :::: burn hex
-::avrdude -c usbtiny -p t13 -U flash:w:"%main%_WinAVR-20100110.hex":a -U lfuse:w:%lfuse%:m  -U hfuse:w:%hfuse%:m
+avrdude -c usbtiny -p %mcu% -U flash:w:%hex%:a -U lfuse:w:%lfuse%:m  -U hfuse:w:%hfuse%:m
 
-avrdude -c usbtiny -p %mcu% -U flash:w:%hex%:a
+::avrdude -c usbtiny -p %mcu% -U flash:w:%hex%:a
 
 :::: avrdude terminal only
 ::avrdude -c usbtiny -p %mcu% -t
